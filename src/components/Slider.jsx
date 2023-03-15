d406bf03082d43b5c9afd2e377c7c3a0b5de3fc6
milkabaldi <justmainc8@gmail.com>
2023-03-15 19:17:39 +0200

    DataBase of slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
+import styles from '/src/styles/styles.js'
+import images from '/src/constants/images'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export function Slider() {
   function handleClick() {
-    console.log('increment like count');
+    console.log('increment like count')
   }
 
   return (
     <section>
-      <div className='banners'>
-        
+      <div className={`${styles.boxWidth} flex flex-col`}>
+        <div className="banners">
+          <div>
+            <Image  src={images.bannerOne} />
+          </div>
+          <div>
+            <Image  src={images.bannerTwo} />
+          </div>
+          <div>
+            <Image  src={images.bannerThree} />
+          </div>
+        </div>
+        <div className="buttons">
+          <button onClick={handleClick} className="bg-dimLate hover:bg-dimMoca transition-colors ease-in-out duration-300 py-5 px-9 flex justify-between gap-2 items-center">
+            Like
+          </button>
+        </div>
       </div>
-      <div className='buttons'>
-        <button onClick={handleClick} className='button'>Like</button>
-      </div>
-      
     </section>
-  );
+  )
 }
-
--- src/constants/images.js
+++ src/constants/images.js
@@ -2,7 +2,7 @@ import NdLogo      from '/src/assets/NdFoodLogo.png'
 import Account     from '/src/assets/account.png'
 import Search      from '/src/assets/search.png'
 import Cart        from '/src/assets/cart.png'
-import bannerONe   from '/src/assets/bannerOne.png'
+import bannerOne   from '/src/assets/bannerOne.png'
 import bannerTwo   from '/src/assets/bannerTwo.png'
 import bannerThree from '/src/assets/bannerThree.png'
 
@@ -13,7 +13,7 @@ export default  {
     Account,
     Search,
     Cart,
-    bannerONe,
+    bannerOne,
     bannerTwo,
     bannerThree
 }
