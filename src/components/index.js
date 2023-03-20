ef2eb49eb71b1ea6a1d2df5d09545946eb8946c6
milkabaldi <justmainc8@gmail.com>
2023-03-20 20:31:54 +0200

    How order

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar.jsx'
 import Footer from './Footer.jsx'
 import Slider from './slider.jsx'
+import HowOrder from './HowOrder.jsx'
 
 
 export default {
     Footer,
     Navbar,
-    Slider
+    Slider,
+    HowOrder,
 }
--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
 import { Slider } from '@/components/Slider'
+import { HowOrder } from '@/components/HowOrder'
 
 export default function Homepage({data}) {
   return (
     <>
         <Navbar data={data} />
-        <Slider data={data} />
+        <Slider />
+        <HowOrder />
         <Footer />
     </>
   )
