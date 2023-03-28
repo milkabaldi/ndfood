b72fdce5a765566be129bf929873558534597413
milkabaldi <justmainc8@gmail.com>
2023-03-28 20:11:12 +0300

    Data Fetching

--- src/components/HowOrder.jsx
+++ src/components/HowOrder.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const HowOrder = ({ mods }) => {
+export const HowOrder = ({ data }) => {
   return (
     <section className='hello'>
       <div>
-      {mods.map((mod) => (
+      {data && data.map((itme) => (
         <div>
-          <span key={mod.title}>{mod.title}</span>
-          <p key={mod.txt}>{mod.txt}</p>
-          <Image src={mod.img} alt="background image" />
+          <span key={item.title}>{item.title}</span>
+          <p key={item.txt}>{item.txt}</p>
+          <Image src={item.img} alt="background image" />
         </div>
 
 ))}
@@ -25,7 +25,7 @@ export async function getStaticProps() {
   console.log(how_order)
   return{
     props: {
-      data: { mods:how_order},
+      data: how_order,
     }
   }
 }
