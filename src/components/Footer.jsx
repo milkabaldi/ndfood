b247f7d6e9ba295cee12929c931251cf89d788eb
milkabaldi <justmainc8@gmail.com>
2023-05-07 20:55:57 +0300

    Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer menu</div>
+    <div>
+      <div>
+        <img />
+        <ul>
+          <li>Магазин</li>
+          <li>Пн-Нед: 9:00 - 19:00</li>
+          <li>Вінницька обл. м. Гайсин вул. 1 Травня 46 Г</li>
+        </ul>     
+      </div>
+      <div>
+        <span>КАТЕГОРІЇ</span>
+        <ul>
+          <li>Алкогольні вироби</li>
+          <li>Напої</li>
+          <li>Снеки</li>
+          <li>Гарячі напої</li>
+          <li>Солодощі</li>
+          <li>Бакалія</li>
+          <li>М’ясні вироби</li>
+          <li>Молочні вироби</li>
+          <li>Вітаміни</li>
+          <li>Побутова хімія</li>
+          <li>Акції</li>
+        </ul>
+      </div>
+      <div>
+        <span>ПОКУПЦЯМ</span>
+        <ul>
+          <li>Доставка та оплата</li>
+          <li>Програма лояльності</li>
+          <li>Подарункові сертифікати</li>
+          <li>Контакти</li>
+        </ul>
+      </div>
+      <div>
+        <p>+38 (096) 122 33 77</p>
+        <span>Facebook</span>
+        <span>Instagram</span>
+      </div>
+    </div>
   )
 }
 
