8806eb5c7e2c299be56117fac620036e38b976e1
milkabald <justmainc8@gmail.com>
2023-05-29 21:24:59 +0300

    test

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,7 +5,7 @@ function main() {
     <main>
        <section className={`${styles.paddingY} ${styles.paddingX} `}>
       <div className={`${styles.boxWidth}`} >
-        Main SSR for page ID just test
+        Main SSR for page ID just another test
         </div>
       </section>
     </main>
