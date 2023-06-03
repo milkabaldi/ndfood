f3e507c9d293b49054502ca2a962d2675172edbf
milkabald <justmainc8@gmail.com>
2023-06-03 19:57:57 +0300

    how order

--- src/components/HowOrder.jsx
+++ src/components/HowOrder.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import data from '../../data/data.json';
+import styles from '/src/styles/styles.js'
 import images from '/src/constants/images'
 
 
