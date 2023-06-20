cadc9f7ccf53a8582af6b4ada6c46fc4ce671ba7
milkabald <justmainc8@gmail.com>
2023-06-20 20:01:52 +0300

    Button

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -21,7 +21,11 @@ export const LogIn = () => {
             <input type="email" name="email" id="email" class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
             <input type="password" name="password" id="password" placeholder="••••••••" class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+            <div>
             <button type="submit" class="bg-dimLate hover:bg-dimMoca transition-colors ease-in-out duration-300 py-5 px-9 text-secondary text-center">ЗБЕРЕГТИ</button>
+            <button type="submit" class="bg-dimLate hover:bg-dimMoca transition-colors ease-in-out duration-300 py-5 px-9 text-secondary text-center">Вийти</button>
+            </div>
+            
         </form>
 
     </div>
