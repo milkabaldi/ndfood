11078422a6cf43a3210de0ca1a0d5626243ccd80
milkabald <justmainc8@gmail.com>
2023-07-01 22:08:10 +0300

    Check

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
-import data from '../../data/data.json';
+import data from '../../data/data.json'
 import styles from '/src/styles/styles.js'
 import images from '/src/constants/images'
 
-
 const Checkout = () => {
   return (
-    <div>
-            <span>Checkout Test</span>
-    </div>
+    <section className={`${styles.paddingY} ${styles.paddingX} bg-secondary`}>
+      <div className={`${styles.boxWidth} `}>Checkout</div>
+    </section>
   )
 }
 
