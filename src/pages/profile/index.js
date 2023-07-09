e6b1f929204f7df73337f379b16920ad26648267
milkabald <justmainc8@gmail.com>
2023-07-09 22:11:03 +0300

    Test Tabs

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {LogIn}  from '@/components/LogIn'
-import {SingIn} from '@/components/SingIn'
+import SingIn from '@/components/SingIn'
 import PasswordReset from '@/components/PasswordReset'
 
 const Profile = () => {
