6681764e92b5502500f606b02b43045cbc55e68e
milkabald <justmainc8@gmail.com>
2023-07-24 20:12:23 +0300

    Sing-in

--- src/components/SingIn.jsx
+++ src/components/SingIn.jsx
@@ -7,7 +7,7 @@ import images from '/src/constants/images'
 
 export default function SingIn() {
   return (
-    <section className='pt-14 pb-20'>
+    <section className='p-20 fixed top-0 right-0 bg-secondary h-full'>
         <form className='flex flex-col w-96 m-auto p-'>
         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" name="email" id="email" class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
