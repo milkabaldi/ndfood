a3f127c940d329f3f6422247e8406cf1f0cce9cf
milkabald <justmainc8@gmail.com>
2023-08-05 10:08:29 +0300

    SingIIn

--- src/components/SingIn.jsx
+++ src/components/SingIn.jsx
@@ -14,11 +14,11 @@ export default function SingIn() {
         <span className='flex gap-2'>Нема аккаунта?<Link href='#' className='text-dimLate hover:text-dimMoca transition-colors ease-in-out duration-300' >Зареєструватися</Link></span>
       </div>
         <form className='flex flex-col w-96 m-auto p-'>
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ім'я</label>
             <input type="password" name="password" id="password" placeholder="Your Name" class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
-        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ваша пошта</label>
             <input type="email" name="email" id="email" class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Пароль</label>
             <input type="password" name="password" id="password" placeholder="••••••••" class="mb-5 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
             <div>
             <button type="submit" class="bg-dimLate hover:bg-dimMoca transition-colors ease-in-out duration-300 py-5 px-9 text-secondary text-center">Реєстрація Testova</button>
