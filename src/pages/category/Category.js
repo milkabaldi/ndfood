c002fcc17b9d9e794c08c34ae82b79181794a949
milkabald <justmainc8@gmail.com>
2023-12-14 11:34:07 +0200

    Not today

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -5,8 +5,8 @@ export const Category = () => {
     <section>
       <h2>Test for me</h2>
       <div>Test block Test </div>
-      <div>block testing</div>
-      <div>block</div>
+      <div>testblock testing</div>
+      <div>test block</div>
     </section>
     
   )
