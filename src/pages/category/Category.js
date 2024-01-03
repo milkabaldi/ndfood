6b23ce0ab48d9b0747f46f6d0c5b22ea2cdab99c
milkabald <justmainc8@gmail.com>
2024-01-03 19:34:22 +0200

    NTD

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Category = () => {
   return (
     <section>
-      <h2>Not today </h2>
+      <h2>Not today NTD</h2>
       <div>Test block Test </div>
       <div>test </div>
       <div>test block test</div>
