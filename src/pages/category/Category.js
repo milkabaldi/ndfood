7b08a409d72a027fabc2a4851c890201ca24c99b
milkabald <justmainc8@gmail.com>
2024-01-05 20:52:57 +0200

    Test

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Category = () => {
   return (
     <section>
-      <h2>1</h2>
+      <h2>2</h2>
       <div>Test block Test </div>
       <div>test </div>
       <div>test block test</div>
