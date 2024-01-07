c3a0e1b874354da472c4a85b9cd4febd73d157ab
milkabald <justmainc8@gmail.com>
2024-01-07 20:37:43 +0200

    Test

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Category = () => {
   return (
     <section>
-      <h2>Test today</h2>
+      <h2>Test today Test </h2>
       <div>Test block Test </div>
       <div>test </div>
       <div>test block test</div>
